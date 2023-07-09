04e0024ec16f4ee653f863a5cddea9f2082555e2
Namrata <namratab24@gmail.com>
2023-07-10 00:53:50 +0530

    added app.yaml

--- src/main/java/com/hackathon/poc/GListener.java
+++ src/main/java/com/hackathon/poc/GListener.java
@@ -1,5 +1,6 @@
 package com.hackathon.poc;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
